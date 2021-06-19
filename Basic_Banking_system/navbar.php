5489ab8b7fbea89c9ca8242e6d319a142a1e9c8c
Pratiksha2462 <86158993+Pratiksha2462@users.noreply.github.com>
2021-06-19 21:09:14 +0530

    Add files via upload

---
+++ Basic_Banking_system/navbar.php
@@ -0,0 +1,19 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+     
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="home.html" style="color :  #8b158f;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact_us.html" style="color :  #8b158f;"><b>Contact us</b></a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="home.html" style="color :  #8b158f;"><b>Back</b></a>
+              </li>
+          </div>
+       </nav>
